3907fc59aae48284a6de8e78f7322f46175bec1a
smeira <samanta.meira@gmail.com>
2020-12-23 19:24:33 -0300

    Add funcionalidade excluir Nota

--- src/components/ListaDeNotas/ListaDeNotas.jsx
+++ src/components/ListaDeNotas/ListaDeNotas.jsx
@@ -11,7 +11,10 @@ class ListaDeNotas extends Component {
                 {this.props.notas.map((nota, index) => {
                     return(
                     <li className="lista-notas_item" key={index}>
-                        <CardNota titulo = {nota.titulo} texto = {nota.texto}/>
+                        <CardNota 
+                        indice = {index}
+                        apagarNota={this.props.apagarNota.bind(this)}
+                        titulo = {nota.titulo} texto = {nota.texto}/>
                     </li>
                     );
                 })}
